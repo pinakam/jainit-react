4deb75564ef253a03e28214c615a1f4d385b999b
JAY VARMA <jayneetvarma.perfettosolutions@gmail.com>
2023-04-19 10:21:53 +0530

    github-demo1

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
