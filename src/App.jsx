821c71a12cf9b9674ebdd86180982c7ab8d76550
JAY VARMA <jayneetvarma.perfettosolutions@gmail.com>
2023-04-19 15:04:00 +0530

    js basic concepts

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
       <div className="container">
         <div className="row">
           <div className="col-12">
-
+            dcskjh,n
           </div>
         </div>
       </div>
